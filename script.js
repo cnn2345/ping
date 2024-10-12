9c496c819bbee4f29b7b64b28971e8467a845ddd
Cheno N <154800279+cnn2345@users.noreply.github.com>
2024-10-12 11:10:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ userData['tgWebAppStartParam'] = tgWebAppStartParam;
 // Display user information in the HTML
 const userInfoElement = document.getElementById('user-info');
 userInfoElement.textContent = `User Info: ${JSON.stringify(userData, null, 2)}`;
-// Log to console for debugging
+
+
 console.log("User Data:", userData);
 console.log("Start Param:", tgWebAppStartParam);
